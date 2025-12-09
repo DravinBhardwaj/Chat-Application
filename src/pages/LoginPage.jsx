7f1b3b20839f562056f41f35ae6fe20146411263
Dravin Bhardwaj <dravinbhardwaj@gmail.com>
2025-12-09 18:37:50 +0530

    Login and SignUp page completed with logic of frontend by Dev Dravin

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,7 +8,15 @@ const LoginPage = () => {
   const[password,setPassword] = useState("");
   const [bio,setBio] = useState("");
   const [isDataSubmitted,setIsDataSubmitted] = useState(false);
+  
+  const onSubmitHandler = (event)=>{
+    event.preventDefault();
 
+    if(currState==='Sign up' && !isDataSubmitted){
+      setIsDataSubmitted(true);
+      return;
+    }
+  }
   return (
 
     <div className='min-h-screen bg-cover bg-center flex items-center
@@ -17,16 +25,16 @@ const LoginPage = () => {
       <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]'/>
 
       {/* {-------right--------} */}
-      <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex 
+      <form onSubmit={onSubmitHandler}className='border-2 bg-white/8 text-white border-gray-500 p-6 flex 
       flex-col gap-6 rounded-lg shadow-lg'>
         <h2 className='font-medium text-2xl flex justify-between items-center'>
           {currState}
-          <img src={assets.arrow_icon} alt="" className='w-5 cursor-pointer'/>
+          {isDataSubmitted && <img onClick={()=>setIsDataSubmitted(false)} src={assets.arrow_icon} alt="" className='w-5 cursor-pointer'/>}
           </h2>
           {
             currState==="Sign up" && !isDataSubmitted &&  (
               
-          <input onChange={(e)=>setEmail(e.target.value)} value={fullName}
+          <input onChange={(e)=>setFullName(e.target.value)} value={fullName}
           type="text" className='p-2 border border-gray-500 rounded-md 
           focus:outline-none' placeholder="Full Name" required/>
             )
@@ -41,7 +49,7 @@ const LoginPage = () => {
               <input onChange={(e)=>setPassword(e.target.value)} value={password}
               type="password" placeholder="password" required
               className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2
-              focus: ring-indigo-500'/>
+              focus:ring-indigo-500'/>
               </>
             )
           }
